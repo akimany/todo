8f754533509e1d16309ed73a63b4093ce494fb40
akimany <akimany@gmail.com>
2018-01-20 00:47:37 +0000

    Clean up

--- js/Remove.js
+++ js/Remove.js
@@ -1,6 +1,4 @@
 // it might be said:
-// import $ from 'jquery'
-// import ElementContent from './ElementContent'
 import PrintElem from './AddDone'
 
 const arr = []
@@ -10,15 +8,6 @@ const Remove = element => {
   elementParent.remove()
   arr.push(elementParent)
   PrintElem(elementParent, doneCont)
-  // while (elementParent !== 'li') {
-  // elementParent = elementParent.parentNode
-  // }
-
-  // removeChild(child);
-
-  //  document.getElementsByClassName('done_cont')[0].appendChild(elementParent)
-  // const elementText = $(element.currentTarget).closest('li').text()
-  // return ElementContent(elementText)
 }
 
 export default Remove
