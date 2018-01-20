00c409966890dc91371bb17a7ea49ec6d19e7cce
akimany <akimany@gmail.com>
2018-01-20 16:55:49 +0000

    Add the latest

--- js/Render.js
+++ js/Render.js
@@ -6,7 +6,6 @@ const Render = () => {
   const $todoInputVal = $('.todo_input').val()
     ? $('.todo_input').val() + Math.ceil(Math.random() * 10)
     : 'empty'
-
   const todoCont = document.getElementsByClassName('todo_cont')[0]
 
   todoCont.innerHTML += `<li class="todoListItem">${$todoInputVal}<span class="remove octicon octicon-x" aria-hidden="true"></span><i className="material-icons md-48">face</i></li>`
