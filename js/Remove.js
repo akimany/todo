a8f7caaeb74a26dad4254888d7f240c7eca42f97
akimany <akimany@gmail.com>
2018-01-25 11:02:28 +0000

    HTML cleanup and icon switch

--- js/Remove.js
+++ js/Remove.js
@@ -8,11 +8,15 @@ const Remove = element => {
     const elementParent = element.target.parentNode
     const elementContainerSwitch =
       elementParent.parentNode.id === 'todo_cont' ? doneCont : todoCont
-    const elementContainer =
-      elementParent.parentNode.id === 'todo_cont' ? todoCont : doneCont
-
+    // const elementContainer = elementParent.parentNode.id === 'todo_cont' ? todoCont : doneCont
     elementParent.remove()
     elementContainerSwitch.appendChild(elementParent)
+
+    if (element.target.parentNode.parentNode.id === 'done_cont') {
+      elementParent.firstChild.nextSibling.innerHTML = 'add_circle_outline'
+    } else if (element.target.parentNode.parentNode.id === 'todo_cont') {
+      elementParent.firstChild.nextSibling.innerHTML = 'remove_circle_outline'
+    }
   }
 }
 
