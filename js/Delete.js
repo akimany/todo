14256b40b9f08610c93a07179bd6bcc87e407d0e
akimany <akimany@gmail.com>
2018-01-29 16:41:19 +0000

    Refactor in progress

--- js/Delete.js
+++ js/Delete.js
@@ -4,9 +4,12 @@ const doneCont = document.getElementsByClassName('done_cont')[0]
 const todoCont = document.getElementsByClassName('todo_cont')[0]
 
 const Delete = element => {
-  const elementContainerSwitch =
-    element.parentNode.id === 'todo_cont' ? doneCont : todoCont
-  elementContainerSwitch.appendChild(element) // check if appendChild removes the node too
+  const elementParentNode = element.target.parentNode
+  if (element.target && element.target.matches('.move')) {
+    const elementContainerSwitch =
+      elementParentNode.parentNode.id === 'todo_cont' ? doneCont : todoCont
+    elementContainerSwitch.appendChild(elementParentNode) // check if appendChild removes the node too
+  }
 }
 
 export default Delete
--- js/Move.js
+++ js/Move.js
@@ -2,14 +2,13 @@
 import Delete from './Delete'
 
 const Move = element => {
-  if (element.target && element.target.matches('.move')) {
-    const elementParent = element.target.parentNode
-    if (element.target.parentNode.parentNode.id === 'done_cont') {
-      elementParent.firstChild.nextSibling.innerHTML = 'add_circle_outline'
-    } else if (element.target.parentNode.parentNode.id === 'todo_cont') {
-      elementParent.firstChild.nextSibling.innerHTML = 'remove_circle_outline'
-    }
-    Delete(elementParent)
+  Delete(element)
+
+  const elementParentNode = element.target.parentNode
+  if (element.target.parentNode.parentNode.id === 'done_cont') {
+    elementParentNode.firstChild.nextSibling.innerHTML = 'add_circle_outline'
+  } else if (element.target.parentNode.parentNode.id === 'todo_cont') {
+    elementParentNode.firstChild.nextSibling.innerHTML = 'remove_circle_outline'
   }
 }
 
