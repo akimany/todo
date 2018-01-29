056018e70b6ae311538fa7547f06bce8b65f7f67
akimany <akimany@gmail.com>
2018-01-29 18:54:44 +0000

    Clean yo

--- js/main.js
+++ js/main.js
@@ -11,14 +11,3 @@ todoCont.addEventListener('click', Move)
 doneCont.addEventListener('click', Move)
 
 todoAdd.addEventListener('click', Render)
-
-// bind events
-
-// it might be said
-// TODOS
-// - clicking on the 'x' removes the item from the list
-// - clicking on the 'x' adds that item to the new list
-// - clicking on the 'x' removes that from the array
-// - make a new array for the 'done' list - done
-
-// Do something with the date and Math functions
