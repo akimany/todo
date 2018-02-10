017025611855f3c7cce5fd499e9ffb6162f13e9d
akimany <akimany@gmail.com>
2018-02-10 14:14:25 -0500

    Input on click

--- js/NewList.js
+++ js/NewList.js
@@ -1,10 +1,11 @@
 // it might be said:
 const buttonContainer = document.getElementsByClassName('button_container')[0]
-const newInput =
+const newInput = document.createElement('input')
+newInput.className = 'form-control col-2'
 
 // in progress - get a button to show an input for a new list on click
 const NewList = () => {
-  buttonContainer.innerHTML = 'test'
+  buttonContainer.appendChild(newInput)
 }
 
 export default NewList
