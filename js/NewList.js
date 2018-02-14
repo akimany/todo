9b83861b9a91ecc1a70c8850c7e9b09f82bb7cca
akimany <akimany@gmail.com>
2018-02-14 15:40:12 +0000

    New list work

--- js/NewList.js
+++ js/NewList.js
@@ -1,11 +1,16 @@
 // it might be said:
 const buttonContainer = document.getElementsByClassName('button_container')[0]
 const newInput = document.createElement('input')
-newInput.className = 'form-control col-2'
+newInput.className = 'form-control'
+
+const newDiv = document.createElement('div')
+newDiv.className = 'col-12 col-sm-9'
+
+newDiv.appendChild(newInput)
 
 // in progress - get a button to show an input for a new list on click
 const NewList = () => {
-  buttonContainer.appendChild(newInput)
+  buttonContainer.appendChild(newDiv)
 }
 
 export default NewList
