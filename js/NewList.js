faad89e399e4484046cb5d892dead06bfcfce12d
akimany <akimany@gmail.com>
2018-02-19 15:23:47 +0000

    Check if it's connected to github or bitbucket

--- js/NewList.js
+++ js/NewList.js
@@ -4,11 +4,10 @@ const newInput = document.createElement('input')
 newInput.className = 'form-control'
 
 const newDiv = document.createElement('div')
-newDiv.className = 'col-12 col-sm-9'
+newDiv.className = 'col-12 col-md-9'
 
 newDiv.appendChild(newInput)
 
-// in progress - get a button to show an input for a new list on click
 const NewList = () => {
   buttonContainer.appendChild(newDiv)
 }
