5128e0052cae53212d0ef7b4ddf0beeee7f74bf3
akimany <akimany@gmail.com>
2018-02-23 15:09:26 +0000

    Object.keys for iterating over localStorage

--- js/LocalStorageWork.js
+++ js/LocalStorageWork.js
@@ -1,11 +1,30 @@
 // it might be said:
-
+// const newList = document.getElementsByClassName('new_list')[0]
+// const addNewList = document.getElementsByClassName('add_new_list')[0]
 const LocalStorageWork = () => {
   // const listName = 'New list' // this should be dynamic, from an input
+  // const newListVal = newList.value
+
   // if other lists are available, option to select and load that list through a select
-  // const $todoInputVal = $('.todo_input').val() ? $('.todo_input').val() : 'empty'
+  const oldLists = []
+  if (oldLists.length > 1) {
+    for (let i = 0; i < oldLists.length; i += 1) {
+      console.log(oldLists)
+    }
+  } else {
+    // addNewList.focus()
+    // return cursor to the add input box
+  }
+
   // check if localstorage has a todo object
   // if it does,
+  if (localStorage.length > 0) {
+    const justLocalStorageObj = Object.keys(localStorage)
+    console.log(justLocalStorageObj)
+    for (let i = 0; i < justLocalStorageObj.length; i += 1) {
+      console.log(localStorage[justLocalStorageObj[i]])
+    }
+  }
   // - for each new list in the object, make a new list
   // - for each list, populate the todo_cont with list items in the array of the list object
   // if it doesn't,
--- js/NewList.js
+++ js/NewList.js
@@ -1,7 +1,7 @@
 // it might be said:
 const buttonContainer = document.getElementsByClassName('button_container')[0]
 const newInput = document.createElement('input')
-newInput.className = 'form-control'
+newInput.className = 'form-control add_new_list'
 
 const newDiv = document.createElement('div')
 newDiv.className = 'col-12 col-md-9'
